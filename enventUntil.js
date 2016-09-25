cb5467cd3bcef26ea78c666c58b98b36d0c8eeb1
Vhyn <jidashehuixue@gmail.com>
2016-09-25 15:15:41 +0800

    Update enventUntil.js
    增加了阻止事件冒泡、阻止默认事件的方法

--- enventUntil.js
+++ enventUntil.js
@@ -1,6 +1,6 @@
-// 事件冒泡跨平台兼容
 
 var enventUtil ={
+			//添加事件
 			addHandler:function(element,type,handler){
 				if (element.addEventListener) {
 					element.addEventListener(type,handler,false);
@@ -17,6 +17,32 @@ var enventUtil ={
 					element.detachEvent("on"+type,handler);
 				}else{
 					element["on"+type]=handler
+				}
+			},
+			//获取事件
+			getEvent:function(event){
+				return event?event:window.event;
+			},
+			getType:function(event){
+				return event.type;
+			},
+			getElement:function(event){
+				return event.target || event.srcElement;
+			},
+			//阻止默认行为
+			preventDefault:function (event) {
+				if (event.preventDefault) {
+					event.preventDefault();
+				} else {
+					event.returnValue=false;
+				}
+			},
+			//阻止冒泡
+			stopPropagation:function(event){
+				if (event.stopPropagation) {
+						event.stopPropagation();//chrome等浏览器
+				} else {
+					event.cancelBubble();//IE 下阻止冒泡
 				}
 			}
 		}
